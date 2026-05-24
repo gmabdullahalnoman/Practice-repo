f890c05acae91d92c32e9e01cc7c2f0c3b2836c9
Abdullah Al Noman <gmabdullahalnoman@gmail.com>
2026-05-24 14:56:52 +0600

    Implemented divisibleSumPairs() using nasted for loop

--- projects/divisibleSumPairs/src/main/java/org/example/Main.java
+++ projects/divisibleSumPairs/src/main/java/org/example/Main.java
@@ -6,45 +6,31 @@ import java.util.List;
 
 class Result {
 
-    /*
-     * Complete the 'divisibleSumPairs' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER k
-     *  3. INTEGER_ARRAY ar
-     */
     // Method divisibleSumPairs(n, k, arr)
     public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
-        // Write your code here
-        /*
-    int count = 0
-    for i = 0 To n - 1
-        for j = i + 1 To n - 1
-            sum = arr[i] + arr[j]
-            if sum MOD k == 0
-                count = count + 1
-            end if
-        end for
-    end for
-         */
-        return 0; //return int count
+
+        int count = 0;
+            for (int i = 0;i < n - 1;i++){
+                for (int j = i + 1; j < n;j++){
+                    int sum = ar.get(i) + ar.get(j);
+                if (sum % k == 0) {
+                    count = count + 1;
+            }//end if
+        }//end inner loop
+    }//end outer loop
+
+        return count; //return int count
     } // end function
 }
 public class Main {
     static void main()throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
+        String[] firstMultipleInput = bufferedReader.readLine().split(" ");
         int n = Integer.parseInt(firstMultipleInput[0]);
-
         int k = Integer.parseInt(firstMultipleInput[1]);
 
-        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
+        String[] arTemp = bufferedReader.readLine().split(" ");
         List<Integer> ar = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
@@ -54,10 +40,9 @@ public class Main {
 
         int result = Result.divisibleSumPairs(n, k, ar);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        System.out.println(result);
+        bufferedReader.close();
 
         bufferedReader.close();
-        bufferedWriter.close();
     }
 }
